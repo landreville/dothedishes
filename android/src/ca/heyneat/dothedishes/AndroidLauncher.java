62a44818cf635465dd6d08aab8d5d29d1431ef4f
Jason <jason@deadtreepages.com>
2017-11-29 21:37:44 -0500

    Remove ads.

--- android/src/ca/heyneat/dothedishes/AndroidLauncher.java
+++ android/src/ca/heyneat/dothedishes/AndroidLauncher.java
@@ -1,25 +1,11 @@
 package ca.heyneat.dothedishes;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
 
 public class AndroidLauncher extends AndroidApplication {
-    private static final String AD_UNIT_ID = "";
-    protected AdView adView;
-    protected View gameView;
-    private boolean enableAds = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,89 +15,7 @@ public class AndroidLauncher extends AndroidApplication {
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
 
-        if(enableAds){
-            // Create a layout with two views, one for libgdx game and one for the ad.
-            createWithAds(cfg);
-        }else {
-            // Just start the damn game.
-            initialize(new DoTheDishes(), cfg);
-        }
-    }
-
-    private void createWithAds(AndroidApplicationConfiguration cfg) {
-        // Do the stuff that initialize() would do for you
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-        RelativeLayout layout = new RelativeLayout(this);
-        LayoutParams params = new LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.MATCH_PARENT
-        );
-        layout.setLayoutParams(params);
-
-        AdView admobView = createAdView();
-        layout.addView(admobView);
-        View gameView = createGameView(cfg);
-        layout.addView(gameView);
-
-        setContentView(layout);
-        startAdvertising(admobView);
-    }
-
-
-    @SuppressLint("ResourceType")
-    private AdView createAdView() {
-        adView = new AdView(this);
-        adView.setAdSize(AdSize.SMART_BANNER);
-        adView.setAdUnitId(AD_UNIT_ID);
-        adView.setId(12345);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT
-        );
-        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-        adView.setLayoutParams(params);
-        adView.setBackgroundColor(Color.BLACK);
-        return adView;
-    }
-
-    private View createGameView(AndroidApplicationConfiguration cfg) {
-        gameView = initializeForView(new DoTheDishes(), cfg);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT
-        );
-        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-        params.addRule(RelativeLayout.ABOVE, adView.getId());
-        gameView.setLayoutParams(params);
-        return gameView;
-    }
-
-    private void startAdvertising(AdView adView) {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) adView.resume();
-    }
-
-    @Override
-    public void onPause() {
-        if (adView != null) adView.pause();
-        super.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        if (adView != null) adView.destroy();
-        super.onDestroy();
+        // Just start the damn game.
+        initialize(new DoTheDishes(), cfg);
     }
 }
